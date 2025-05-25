9bf88a46b46c0b62877e20bee305db3649c8c235
Ar. Diamanti <ariadiamanti@gmail.com>
2025-05-25 21:01:46 +0300

    feat: new components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+import Layout from "./components/Layout.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import Counter from "./components/Counter.tsx";
 
 function App() {
 
-  return (
-    <>
-       {/*<ClassComponent/>*/}
-       {/* <FunctionalComponent/>*/}
-       {/* <ArrowFunctionalComponent/>*/}
-        <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                {/*<ClassComponent/>*/}
+                {/*<FunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>*/}
+                {/*<ArrowFunctionalComponentWithPropsType*/}
+                {/*  title="Is an Arrow Functional Component with Props!"*/}
+                {/*  description="this is a description"*/}
+                {/*/>*/}
+
+                {/*<ClassComponentWithState/>*/}
+                {/*<FunctionalComponentWithState/>*/}
+                <Counter/>
+
+
+            </Layout>
+        </>
+    )
 }
 
 export default App
