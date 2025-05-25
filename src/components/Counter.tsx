2851fd38660db8fe8faade32f6c48a1dbe21e1d3
Ar. Diamanti <ariadiamanti@gmail.com>
2025-05-25 21:29:13 +0300

    feat: custom colors created

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -23,7 +23,7 @@ const Counter = ( ) => {
             <div className="space-y-4 text-2xl pt-12">
                 <h1 className="text-center">Count is {count}</h1>
                 <div className="text-center space-x-4">
-                    <CounterButton onClick={increaseCount} label="Increase" />
+                    <CounterButton onClick={increaseCount} label="Increase" addClass="bg-green-500"/>
                     <CounterButton onClick={decreaseCount} disabled={count === 0} label="Decrease" />
                     <CounterButton onClick={resetCount} disabled={count === 0} label="Reset" addClass="bg-cf-dark-red"/>
                 </div>
